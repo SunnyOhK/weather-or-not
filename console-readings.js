677cfc120183c0debd4e49698e0556cb2a3401e0
SunnyOhK <127900916+SunnyOhK@users.noreply.github.com>
2023-04-24 19:34:14 -0500

    got city, state to append to the page

---
+++ console-readings.js
@@ -0,0 +1,16 @@
+JS line47 (myCity) =
+{ name: 'Herbert', lat: 29.5894017, lon: -95.5938342, country: 'US', state: 'Texas' }
+
+country: "US"
+lat: 29.5894017
+lon: -95.5938342
+name: "Herbert"
+state: "Texas"
+
+JS Line65 (dataStore):
+(4)['Manchester', 'Herbert', 'Small', 'Bern']
+0: "Manchester"
+1: "Herbert"
+2: "Small"
+3: "Bern"
+length: 4
